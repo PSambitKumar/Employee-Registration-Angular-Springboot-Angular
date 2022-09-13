457fdce9495710413105b614e523b164fcdfb36c
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-09-13 15:38:40 +0530

    Validation using Angular Reactive Form

--- EmployeeAngularApp/src/app/student/student.component.ts
+++ EmployeeAngularApp/src/app/student/student.component.ts
@@ -19,6 +19,9 @@ export class StudentComponent implements OnInit {
 
   studentFormGroup = new FormGroup({
     name : new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern("^[a-zA-Z ]*$")]),
+    fatherName : new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern("^[a-zA-Z ]*$")]),
+    age : new FormControl("", [Validators.required, Validators.min(5), Validators.max(60), Validators.pattern("^[0-9]*$")]),
+    gender : new FormControl("", [Validators.required]),
     password : new FormControl("", [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$@!%&*?])[A-Za-z\\d#$@!%&*?]{8,30}$")]),
     confirmPassword : new FormControl("", [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$@!%&*?])[A-Za-z\\d#$@!%&*?]{8,30}$")])
   })
@@ -37,15 +40,12 @@ export class StudentComponent implements OnInit {
     }
   }
 
-  get name() : FormControl{
-    return this.studentFormGroup.get("name") as FormControl;
-  }
-  get password() : FormControl{
-    return this.studentFormGroup.get("password") as FormControl;
-  }
-  get confirmPassword() : FormControl{
-    return this.studentFormGroup.get("confirmPassword") as FormControl;
-  }
+  get name() : FormControl{return this.studentFormGroup.get("name") as FormControl;}
+  get fatherName() : FormControl{return this.studentFormGroup.get("fatherName") as FormControl;}
+  get age() : FormControl{return this.studentFormGroup.get("age") as FormControl;}
+  get gender() : FormControl{return this.studentFormGroup.get("gender") as FormControl;}
+  get password() : FormControl{return this.studentFormGroup.get("password") as FormControl;}
+  get confirmPassword() : FormControl{return this.studentFormGroup.get("confirmPassword") as FormControl;}
 
 
   passwordMatchValidator() {
